9514765d8f1cdf077938b7f81d9e343e9123ed4b
kalphageek <kalphageek@outlook.com>
2020-12-28 22:30:57 +0900

    first commit

---
+++ src/main/java/me/kalpha/restapispring/RestapiSpringApplication.java
@@ -0,0 +1,13 @@
+package me.kalpha.restapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kalpha/restapispring/RestapiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalpha.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
